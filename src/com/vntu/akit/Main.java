69b9081ba0bda720c3e3aa49014fdffec77dcf8e
romanbffff <rsirachenko@gmail.com>
2022-03-24 21:32:05 +0200

    v.50%

--- src/com/vntu/akit/Main.java
+++ src/com/vntu/akit/Main.java
@@ -1,8 +1,36 @@
 package com.vntu.akit;
 
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
+    static int var = 2;
 
+    public static void main(String[] args) {
+        int number;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        number = scanner.nextInt();
+        checkIsItANaturalNumber(number);
+        for (int i = 1; i <= 10000; i++) {
+            checkIsItANaturalNumber(i);
+        }
+    }
+    private static void checkIsItANaturalNumber(int n) {
+        if (n > 1) {
+            if (n % var != 0) {
+                var++;
+                checkIsItANaturalNumber(n);
+            } else if (n % var == 0) {
+                print(var, n);
+            }
+        }
+    }
+    private static void print(int var, int n) {
+        if (var == n) {
+            System.out.println(n + " is a natural number");
+        } else {
+            System.out.println(n + " is not a natural number");
+        }
     }
 }
